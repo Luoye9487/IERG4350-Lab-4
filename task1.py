4983fcf8fddda87bcbf70cefebd4062d30d80f6c
CHEN Yee Chiu <chenyeechiu0910@gmail.com>
2025-02-25 04:59:30 +0800

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+def check_name(name):
+    # TODO: implement this function
+    pass
+        
+
+def check_name_len(name):
+    # TODO: implement this function
+    pass
+
+def check_sid_len(name):
+    # TODO: implement this function
+    pass
