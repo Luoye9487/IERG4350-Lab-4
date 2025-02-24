a098133de67549e1222246ec5b173439c422c84b
CHEN Yee Chiu <chenyeechiu0910@gmail.com>
2025-02-25 05:03:30 +0800

    Task 1

--- task1.py
+++ task1.py
@@ -1,12 +1,25 @@
 def check_name(name):
     # TODO: implement this function
-    pass
-        
-
+    if u"\u2b55" in name:
+        return False
+    elif name[0] == " " or name[-1] == " ":
+        return False
+    else:
+        return True
+            
 def check_name_len(name):
     # TODO: implement this function
-    pass
+    if len(name) < 21:
+        return True
+    return False
 
 def check_sid_len(name):
     # TODO: implement this function
-    pass
+    new_name = str(name)
+    if len(new_name) != 10:
+        return False
+    elif " " in new_name:
+        return False
+    elif new_name[0:4] != "1155":
+        return False
+    return True
