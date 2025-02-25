5268a3027b0208d572ef169e1c97dca610f81da5
CHEN Yee Chiu <chenyeechiu0910@gmail.com>
2025-02-25 14:01:01 +0800

    Task 1 new

--- task1.py
+++ task1.py
@@ -1,6 +1,17 @@
+import re
+
 def check_name(name):
     # TODO: implement this function
-    if u"\u2b55" in name:
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  
+        u"\U0001F300-\U0001F5FF"  
+        u"\U0001F680-\U0001F6FF"  
+        u"\U0001F1E0-\U0001F1FF"  
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        "]+", flags=re.UNICODE)
+    
+    if emoji_pattern.search(name):
         return False
     elif name[0] == " " or name[-1] == " ":
         return False
